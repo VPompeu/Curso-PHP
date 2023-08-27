c3fe3554d03b6ae939342a46baf3a0e2c8da02eb
Victor Pompeu <vd.pompeu@gmail.com>
2023-08-27 18:22:20 -0300

    first php

---
+++ ex00/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro php</title>
+</head>
+<body>
+    
+    <h1>
+        <?php 
+            echo "Olá, Mundo! \u{1F30E}";
+        ?>
+    </h1>
+    <p>Primeira aula de php</p>
+
+</body>
+</html>
